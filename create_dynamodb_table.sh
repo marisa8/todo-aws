e80c418331d7662b1bebeb793d835c595cd4d273
marisa mari <goemon758@gmail.com>
2025-07-03 16:15:56 +0900

    Add AWS CLI script to create DynamoDB Todos table

--- create_dynamodb_table.sh
+++ create_dynamodb_table.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+# -----------------------------
+# AWS CLI Script to create DynamoDB Todos Table
+# Author: (cicd_handson)
+# Date: (2025/07/03)
+# -----------------------------
+
+# Variables
+TABLE_NAME="Todos"
+PROFILE="cicd_handson"
+REGION="ap-northeast-1"
+
+echo "Creating DynamoDB table: $TABLE_NAME"
+
 aws dynamodb create-table \
-  --table-name Todos \
+  --table-name $TABLE_NAME \
   --attribute-definitions AttributeName=id,AttributeType=S \
   --key-schema AttributeName=id,KeyType=HASH \
-  --billing-mode PAY_PER_REQUEST
+  --billing-mode PAY_PER_REQUEST \
+  --region $REGION \
+  --profile $PROFILE
+
+echo "Table creation command issued. Use describe-table to check status:"
+echo "aws dynamodb describe-table --table-name $TABLE_NAME --region $REGION --profile $PROFILE"
---
+++ create_dynamodb_table_command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+aws dynamodb create-table \
+  --table-name Todos \
+  --attribute-definitions AttributeName=id,AttributeType=S \
+  --key-schema AttributeName=id,KeyType=HASH \
+  --billing-mode PAY_PER_REQUEST \
+  --profile cicd_handson
